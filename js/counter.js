66197d3d50bc63fe31c487d47c3897ab8faaa724
FounderCasey <caseywilcox@outlook.com>
2017-10-09 10:10:33 -0700

    Added reset button
    added a reset button. I still need to get the positioning correct.

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,7 @@
 var info = document.getElementById("tally-count");
 var minusButton = document.getElementById("minus");
 var plusButton = document.getElementById("plus");
+var resetButton = document.getElementById("reset");
 var count = 0;
 
 minusButton.onclick = function () {
@@ -13,4 +14,12 @@ plusButton.onclick = function () {
   "use strict";
   count += 1;
   info.innerHTML = count;
+};
+
+resetButton.onclick = function () {
+  "use strict";
+  if (window.confirm("Are you sure you want to reset?") === true) {
+    count = 0;
+    info.innerHTML = count;
+  }
 };
